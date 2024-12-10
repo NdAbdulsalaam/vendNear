9d154005e25765c808c19ae47d639bfafd79049e
NdAbdulsalaam <ndabdulsalaam@gmail.com>
2024-12-10 21:27:22 +0100

    finish project basic setup

--- backend/vendNear/settings.py
+++ backend/vendNear/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # install apps
+    'rest_framework',
+    
+    # project apps
 ]
 
 MIDDLEWARE = [
@@ -73,10 +78,21 @@ WSGI_APPLICATION = 'vendNear.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vendNear-db',
+        'USER': 'postgres',
+        'PASSWORD': 'Ndsalaam@1913',
+        'HOST': 'localhost',           # Use the appropriate host (localhost or IP)
+        'PORT': '5432',          
     }
 }
 
