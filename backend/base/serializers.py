a7b7244bc39ed61d69b6942830828a2a3c22422d
NdAbdulsalaam <ndabdulsalaam@gmail.com>
2024-12-11 18:01:15 +0100

    use APIViews and depth

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -5,8 +5,16 @@ class VendorSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Vendor
         fields = ['id', 'user', 'address']
+    
+    def __init__(self, *args, **kwargs):
+        super(VendorSerializer, self).__init__(*args, **kwargs)
+        self.Meta.depth = 1
         
 class VendorDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Vendor
         fields = ['id', 'user', 'address']
+        
+    def __init__(self, *args, **kwargs):
+        super(VendorDetailSerializer, self).__init__(*args, **kwargs)
+        self.Meta.depth = 1
--- backend/base/views.py
+++ backend/base/views.py
@@ -2,10 +2,10 @@ from rest_framework import generics, permissions
 from .serializers import VendorSerializer, VendorDetailSerializer
 from .models import Vendor
 
-class VendorList(generics.ListAPIView):
+class VendorList(generics.ListCreateAPIView):
     queryset = Vendor.objects.all()
     serializer_class = VendorSerializer
 
-class VendorDetail(generics.RetrieveAPIView):
+class VendorDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Vendor.objects.all()
     serializer_class = VendorDetailSerializer
